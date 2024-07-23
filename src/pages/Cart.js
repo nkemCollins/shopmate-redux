bed56ad956ce7f4daea94cc9cba8ba4b69458046
lekecollins <lekecollins2@gmail.com>
2024-07-23 00:32:31 -0400

    Integrated Redux To Shopmate

---
+++ src/pages/Cart.js
@@ -0,0 +1,22 @@
+import { CartCard } from "../components";
+import { useTitle } from "../hooks/useTitle";
+import { useSelector } from "react-redux";
+
+export const Cart = () => {
+  useTitle("Cart");
+  const cartList = useSelector((state) => state.cartState.cartList);
+  const total = useSelector((state) => state.cartState.total);
+
+  return (
+    <main>
+      <section className="cart">
+        <h1>
+          Cart Items: {cartList.length} / ${total}
+        </h1>
+        {cartList.map((product) => (
+          <CartCard key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
+  );
+};
